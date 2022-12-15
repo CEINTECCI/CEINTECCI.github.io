c55d9ff2d7bcef0cb535b1f16ccf719575b7db17
J4ngel <andres_angel@live.com.ar>
2022-12-15 17:09:14 -0500

    Continuación del desarrollo de la vista conócenos  * Se crearon tres archivos con información de personas diferentes para las pruebas  * Se hizo un backup de la vista conócenos existentes y se empezó a desarrollar dicha vista en el archivo cono.html  * Se renombró el script responsable de la lectura de los archivos como ‘cards_information.js’

--- static/js/cards_information.js
+++ static/js/cards_information.js
@@ -6,7 +6,30 @@ function load_info(person) {
     txtFile.send(null);
 
     let information = txtFile.responseText.split("\r\n");
-    let nombreContainer = document.getElementById("nombre")
-    nombreContainer.innerText = information[0]
+    let name = document.getElementById(`${person}_name`)
+    let job = document.getElementById(`${person}_job`)
+    let info = document.getElementById(`${person}_info`)
+    let email = document.getElementById(`${person}_em`)
+
+    name.innerText = information[0]
+    job.innerText = information[1]
+    info.innerText = information[2]
+    email.innerHTML = `<b>Correo Eléctronico:</b><br>${information[3]}`
+}
+
+const generic_id = "person";
+let n_person = 1;
+let temp = "";
+
+while (true) {
+    temp = generic_id + n_person;
+    if (document.getElementById(`${temp}_name`) !== null){
+        // console.log(temp)
+        load_info(temp);
+        n_person += 1
+    }else{
+        // console.log("Salio"+temp)
+        break;
+    }
 }
 
